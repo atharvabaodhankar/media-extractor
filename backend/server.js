c10b15fda7e2b45828018d64b9928240c5e30d15
atharva <baodhankaratharva@gmail.com>
2025-06-19 18:09:50 +0530

    Backend Port Update

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const allowedOrigins = process.env.ALLOWED_ORIGIN.split(',');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors({
   origin: (origin, callback) => {
